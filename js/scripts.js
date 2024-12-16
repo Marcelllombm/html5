449afb1d4740b530cbfd2c9eeed1b50e227ae8ca
Marcelo Borges <melombm@hotmail.com>
2024-12-16 13:58:52 -0300

    menu supenso e voltar ao topo

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,25 @@ $(function(){
     // DESLIZA O MENU
     $('.main_menu a[class!="special"]').click(function(){
         let goto = $('.' + $(this).attr('href').replace("#","")).position().top;
-        $('html, body').animate({scrollTop: goto}, 1000)
-        console.log(goto)
+        $('html, body').animate({scrollTop: goto -  $('.main_header').outerHeight() }, 1000)
         return false;
+    })
+
+    // SUSPENDE MENU
+    $(window).scroll(function(){
+        if($(this).scrollTop() > $('.main_header').outerHeight() + 150){
+            $('body').css('padding-top',$('.main_header').outerHeight() )
+            $('.main_header').addClass('main_header_fixed')
+            $('.j_back').fadeIn(500)
+        }else{
+            $('body').css('padding-top', '0'),
+            $('.main_header').removeClass('main_header_fixed')
+            $('.j_back').fadeOut(500)
+        }
+    })
+
+    // BACK TOPO
+    $('.j_back').click(function(){
+        $('html, body').animate({scrollTop: 0 }, 1000)
     })
 })
